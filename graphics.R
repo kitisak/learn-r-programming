9c9bcd8e0b98d39a96994b913a78d69eb3020ca8
somkiat <somkiat.p@gmail.com>
2016-05-27 15:04:04 +0700

    Working with graphics such as plot, hist, line and points

---
+++ graphics.R
@@ -0,0 +1,9 @@
+plot(rnorm(100), type = 'l', col='red')
+plot(rnorm(100), type = 'l', lwd=3, col='pink')
+
+#### Plot and points ####
+plot(rnorm(100), type = 'p', lwd=3, col='pink', pch=1)
+points(rnorm(100), pch=20)
+
+#### Histogram ####
+hist(rnorm(1000))
