a8a76b3c40a00513dc76aab9f8852d611ab12856
somkiat <somkiat.p@gmail.com>
2016-05-27 15:06:43 +0700

    Change color of graph to red

--- graphics.R
+++ graphics.R
@@ -3,7 +3,7 @@ plot(rnorm(100), type = 'l', lwd=3, col='pink')
 
 #### Plot and points ####
 plot(rnorm(100), type = 'p', lwd=3, col='pink', pch=1)
-points(rnorm(100), pch=20)
+points(rnorm(100), pch=20, col='red')
 
 #### Histogram ####
 hist(rnorm(1000))
