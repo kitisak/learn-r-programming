5a4dc755156db2aa0e99ee455fb9364c578d6a3a
somkiat <somkiat.p@gmail.com>
2016-05-27 11:13:12 +0700

    Learn data type => scala, vector and matrix

---
+++ data_type.R
@@ -0,0 +1,14 @@
+#### Scala ####
+a
+
+
+#### Vector ####
+vector_a = c(1, 2, 3, 4, 5)
+vector_b = c(10, 20, 30, 40, 50)
+print(vector_a)
+print(vector_b)
+
+#### Matrix ####
+a = c(1, 2, 3, 4, 5, 6)
+matrix_a = matrix(a, nrow=3)
+print(matrix_a[3, 2])  #6
